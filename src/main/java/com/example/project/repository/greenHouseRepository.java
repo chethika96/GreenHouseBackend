643c8a19f86f8f176f4481faf59ce105f6b07f6e
chethika96 <chethikawarnakula119@gmail.com>
2024-02-24 03:11:08 +0900

    Remove existing code

--- src/main/java/com/example/project/repository/greenHouseRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.project.repository;
-
-import com.example.project.model.greenHouse;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface greenHouseRepository extends JpaRepository<greenHouse,String>{
-}
